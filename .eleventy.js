cfde49157dbbe36bcc8eec8ae217dca73354034f
Justine <jsalangsang@blacksunplc.com>
2023-08-22 11:33:45 +0800

    'cloudcannon'

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,18 @@
+const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const MarkdownIt = require("markdown-it"),
+  md = new MarkdownIt({
+    html: true,
+  });
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPlugin(
+    pluginBookshop({
+      bookshopLocations: ["component-library"],
+      pathPrefix: "",
+    })
+  );
+  
+  eleventyConfig.addFilter("markdownify", (markdown) => md.render(markdown));
 
   return {
     passthroughFileCopy: true,
@@ -9,6 +22,7 @@ module.exports = function (eleventyConfig) {
       input: "src",
       output: "_site",
       include: "includes",
+      pages: "pages",
     },
   };
 };
